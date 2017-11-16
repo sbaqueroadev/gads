ee073ede81ce90c7b4fdb8a26d782df0ee868baf
sbaqueroadev <30293161+sbaqueroadev@users.noreply.github.com>
2017-11-16 13:25:55 -0500

    Initial Commit

---
+++ src/main/java/co/com/sbaqueroa/gdas/GdasApplication.java
@@ -0,0 +1,12 @@
+package co.com.sbaqueroa.gdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdasApplication.class, args);
+	}
+}
---
+++ src/test/java/co/com/sbaqueroa/gdas/GdasApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.sbaqueroa.gdas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GdasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
