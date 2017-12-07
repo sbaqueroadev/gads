46ae09b1f6b77b4ac24ce3dd63e72705609fe656
Grupo ASD <Grupo ASD@C46ASDDESAP3007.adgasd32.int>
2017-12-07 11:45:20 -0500

    Video sockets

---
+++ src/main/java/co/com/sbaqueroa/gads/webservices/ChatController.java
@@ -0,0 +1,21 @@
+package co.com.sbaqueroa.gads.webservices;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.webjars.RequireJS;
+
+@Controller
+public class ChatController {
+	
+	@MessageMapping("/write")
+	@SendTo("/topic/reading")
+	public String gettingData(String data) throws Exception{
+		return data+" received";
+	}
+	
+}
